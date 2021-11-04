82db437763b016a6d13d3607a54333075909c9a2
Knut Johannes Dahle <knut.johannes.dahle@gmail.com>
2021-11-04 17:55:24 +0100

    Added initial resource group

---
+++ config.tf
@@ -0,0 +1,13 @@
+# Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source ="hashicorp/azurerm"
+      version ="2.83.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+ variable "resource_group_name" {
+  description = "Name of the azure resource group in which you will deploy this template."
+  default     = "rg-kjd-workshop-gruppe4"
+}
+
+variable "resource_group_location" {
+  description = "Location of the azure resource group."
+  default     = "norwayeast"
+}
